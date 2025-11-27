136dccbcc923183e442323df9f3a589f43c8cd64
Anushaamar111 <anushaamar1111@gmail.com>
2025-11-27 20:45:24 +0530

    Add Vercel deployment configuration

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import Product from '../models/Product.js';
-import User from '../models/User.js';
 import { authenticate, verifyCampus } from '../middleware/auth.js';
 
 const router = express.Router();
